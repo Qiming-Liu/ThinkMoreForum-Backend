e82dbed309eeba190a2df4a931bfa3554a94d165
Zhaotong Cui <damwcs@gmail.com>
2022-02-07 14:13:44 +1030

    get name and email from web

--- src/main/java/com/thinkmore/forum/controller/TestController.java
+++ src/main/java/com/thinkmore/forum/controller/TestController.java
@@ -6,10 +6,22 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api/v1/test")
+@RequestMapping(path = "/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 public class TestController {
+
+    @RequestMapping(value="/authentication/unique_username",method= RequestMethod.GET)
+    public String virtify_name(@RequestParam("name") String name) {
+        return "the name is:" + name;
+
+    }
+
+    @RequestMapping(value="/authentication/unique_email",method= RequestMethod.GET)
+    public String virtify_email(@RequestParam("email") String email) {
+        return "the email is:" + email;
+
+    }
 
     @GetMapping(path = "/hello_world")
     public String helloWorld() {
