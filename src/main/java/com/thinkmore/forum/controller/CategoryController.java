ff4ca869df547d0c9eea85cafb710b2a2144eec8
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-02-12 22:43:38 +1100

    feat: user can create update delete find category

--- src/main/java/com/thinkmore/forum/controller/CategoryController.java
+++ src/main/java/com/thinkmore/forum/controller/CategoryController.java
@@ -1,14 +1,15 @@
 package com.thinkmore.forum.controller;
 
 import com.thinkmore.forum.dto.category.CategoryGetDto;
+import com.thinkmore.forum.dto.category.CategoryPutDto;
 import com.thinkmore.forum.service.CategoryService;
+import com.thinkmore.forum.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/v1/category")
@@ -20,6 +21,22 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<List<CategoryGetDto>> findAll() {
         return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @PostMapping(path = "/addCategory/{title}/{description}/{color}")
+    public CategoryGetDto addCategory(@PathVariable("title") String title, @PathVariable("description") String description, @PathVariable("color") String color) {
+        return categoryService.addCategory(title, description, color);
+    }
+
+    @PutMapping
+    public ResponseEntity<Boolean> changeCategory(@RequestBody CategoryPutDto categoryPutDto) {
+        return ResponseEntity.ok(categoryService.changedCategory(categoryPutDto));
+    }
+
+    @DeleteMapping
+    public void deleteCategory(@RequestParam String category_id){
+        UUID uuid = UUID.fromString(category_id);
+        categoryService.deleteCategory(uuid);
     }
 
 }
--- src/main/java/com/thinkmore/forum/service/CategoryService.java
+++ src/main/java/com/thinkmore/forum/service/CategoryService.java
@@ -1,12 +1,19 @@
 package com.thinkmore.forum.service;
 
 import com.thinkmore.forum.dto.category.CategoryGetDto;
+import com.thinkmore.forum.dto.category.CategoryPutDto;
+import com.thinkmore.forum.entity.Category;
+import com.thinkmore.forum.exception.CategoryNotFoundException;
 import com.thinkmore.forum.mapper.CategoryMapper;
 import com.thinkmore.forum.repository.CategoryRepository;
+import com.thinkmore.forum.util.Util;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,6 +24,54 @@ public class CategoryService {
 
     private final CategoryMapper categoryMapper;
 
+
+    @Transactional
+    public CategoryGetDto addCategory (String title, String description, String color) {
+        Category category = new Category();
+
+        int nowSortOrder = getAllCategories().size();
+
+
+        category.setTitle(title);
+        category.setDescription(description);
+        category.setColor(color);
+        category.setProfileImg(null);
+        category.setPostCount(0);
+        category.setSortOrder(nowSortOrder);
+        category.setPinPost(null);
+
+
+
+        categoryRepo.save(category);
+
+
+        return categoryMapper.fromEntity(category);
+    }
+
+    @Transactional
+    public CategoryGetDto getCategoryById(UUID id) {
+
+        return categoryMapper.fromEntity(categoryRepo.findById(id).orElseThrow(() ->
+                new CategoryNotFoundException(String.format("Category %s not found", id))));
+    }
+
+    @Transactional
+
+    public void deleteCategory(UUID uuid) {
+        categoryRepo.deleteById(uuid);
+        System.out.println("success delete");
+    }
+
+    @Transactional
+    public boolean changedCategory(CategoryPutDto categoryPutDto) {
+
+        Category oldCategory = categoryRepo.findById(categoryPutDto.getId())
+                .orElseThrow(() -> new CategoryNotFoundException("Invalid Title"));
+
+        oldCategory.setDescription(String.valueOf(categoryPutDto));
+        categoryMapper.copy(categoryPutDto, oldCategory);
+        return true;
+    }
     public List<CategoryGetDto> getAllCategories() {
 
         return categoryRepo.findAll().stream()
