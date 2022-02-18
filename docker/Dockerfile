b54d341bd2ee2be5d666482d9e5ffbf30f959950
Qiming-Liu <prossliu@gmail.com>
2022-02-18 18:59:53 +1030

    feat: pipeline

---
+++ docker/Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:11
+
+WORKDIR /app
+
+COPY . /app
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/com/thinkmore/forum/ForumApplicationTests.java
+++ src/test/java/com/thinkmore/forum/ForumApplicationTests.java
@@ -1,13 +1,8 @@
 package com.thinkmore.forum;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ForumApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
