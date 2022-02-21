c7cf0465746ba4f139999a78c4e8da1653a15027
Qiming-Liu <prossliu@gmail.com>
2022-02-21 14:38:05 +1030

    chore: config

--- src/main/java/com/thinkmore/forum/configuration/Config.java
+++ src/main/java/com/thinkmore/forum/configuration/Config.java
@@ -1,7 +1,5 @@
 package com.thinkmore.forum.configuration;
 
-import java.time.LocalDate;
-
 public class Config {
     // Jwt
     public final static String[] ignoreUrl = new String[]{
@@ -21,7 +19,7 @@ public class Config {
             "/v1/img/upload",
     };
     public final static String JwtSecretKey = "fruue37r7yrfhf87f7876guyggf%$$#$%^&%RTHGhjjkj23456rkkkkdkd";
-    public final static String JwtPrefix = "";//Bearer ";
+    public final static String JwtPrefix = "Bearer ";
 
     // Role
     public final static String DefaultRole = "unverified_user";
@@ -30,9 +28,9 @@ public class Config {
     public final static String DecodedKey = "1a53d4469f513e1ae3856fc2c603b8d6";
     public final static String Apikey = "bf7429164ac97e5dae68b01c9b5f4db2fdf172b5e843d2c2d8f3f829e9b785c02cd38bda9be56537804f8e5626eeca116989564aa0d603c40355d037f4713c55c2638ef376ce12e5455e4dbfd5cd49cc";
     public final static String fromEmail = "jiangjianglovezhou@gmail.com";
-    public final static String ResetPasswordUrl = "前端url?token=";
+    public final static String ResetPasswordUrl = "https://thinkmoreforum.vercel.app/reset-password?token=";
     public final static String ResetPasswordContext = "Please click the link to reset your new password:\n";
-    public final static String VerifyEmailUrl = "前端url?token=";
+    public final static String VerifyEmailUrl = "https://thinkmoreforum.vercel.app/verify-email?token=";
     public final static String VerifyEmailContext = "Please click the link to verify your new email:\n";
 
     // Oss
