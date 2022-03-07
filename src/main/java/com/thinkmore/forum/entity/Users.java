898a2a5e41b353cb1100460fa50f52527a915360
Alan <68600416+Qiming-Liu@users.noreply.github.com>
2022-03-07 21:25:19 +1030

    Tmf 163 public url no jwt (#46)
    * fix: public url no jwt
    
    * fix: url

--- src/main/java/com/thinkmore/forum/entity/Users.java
+++ src/main/java/com/thinkmore/forum/entity/Users.java
@@ -2,8 +2,10 @@ package com.thinkmore.forum.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.OffsetDateTime;
