a83df70d180e1ccca257b96d700241be28ee3d0e
jiangjiangzhouzhou <67827176+jiangjiangzhouzhou@users.noreply.github.com>
2022-03-26 21:55:42 +1100

    feat: change user's role when a role is deleted (#79)

--- src/main/java/com/thinkmore/forum/repository/UsersRepository.java
+++ src/main/java/com/thinkmore/forum/repository/UsersRepository.java
@@ -1,8 +1,10 @@
 package com.thinkmore.forum.repository;
 
+import com.thinkmore.forum.entity.Roles;
 import com.thinkmore.forum.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +13,5 @@ public interface UsersRepository  extends JpaRepository<Users, UUID> {
     Optional<Users> findByUsername(String username);
 
     Optional<Users> findByEmail(String email);
+    List<Users> findByRole(Roles role);
 }
