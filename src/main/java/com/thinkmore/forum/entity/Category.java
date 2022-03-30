edb8a64935b5453a93ba4fb6f9637dcbb9f6b39f
Melissa Tan <62129448+melissatzt@users.noreply.github.com>
2022-03-30 21:06:52 +1000

    fix: update category description (#89)

--- src/main/java/com/thinkmore/forum/entity/Category.java
+++ src/main/java/com/thinkmore/forum/entity/Category.java
@@ -34,7 +34,7 @@ public class Category {
     @Column(name = "title", nullable = false, length = 20)
     private String title;
 
-    @Column(name = "description", length = 100)
+    @Column(name = "description", length = 255)
     private String description;
 
     @Column(name = "view_count")
