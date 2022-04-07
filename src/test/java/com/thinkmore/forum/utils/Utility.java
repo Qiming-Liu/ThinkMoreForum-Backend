7b146bfb1952de5b229cc434898a8b5a3ae76a91
jiangjiangzhouzhou <jiangjianglovezhou@gmail.com>
2022-04-07 22:50:01 +1000

    feat: "tests"

---
+++ src/test/java/com/thinkmore/forum/utils/Utility.java
@@ -0,0 +1,27 @@
+package com.thinkmore.forum.utils;
+
+import com.thinkmore.forum.dto.roles.RolesGetDto;
+import com.thinkmore.forum.entity.Roles;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+
+@Configuration
+public class Utility {
+
+    public RolesGetDto buildRolesGetDto (UUID id, String roleName, String permission) {
+        RolesGetDto rolesGetDto = new RolesGetDto();
+        rolesGetDto.setId(id);
+        rolesGetDto.setRoleName(roleName);
+        rolesGetDto.setPermission(permission);
+        return rolesGetDto;
+    }
+
+    public Roles buildRoles (UUID id, String roleName, String permission) {
+        Roles roles = new Roles();
+        roles.setId(id);
+        roles.setRoleName(roleName);
+        roles.setPermission(permission);
+        return roles;
+    }
+}
