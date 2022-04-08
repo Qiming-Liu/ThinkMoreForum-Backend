fb8c83eca9e4266db6b06efa982953dc4bc15e8d
jiangjiangzhouzhou <jiangjianglovezhou@gmail.com>
2022-04-08 10:34:02 +1000

    feat: "test failed fixed"

--- src/test/java/com/thinkmore/forum/ForumApplicationTests.java
+++ src/test/java/com/thinkmore/forum/ForumApplicationTests.java
@@ -1,8 +1,12 @@
 package com.thinkmore.forum;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ForumApplicationTests {
 
+    @Test
+    void contextLoads() {
+    }
 }
